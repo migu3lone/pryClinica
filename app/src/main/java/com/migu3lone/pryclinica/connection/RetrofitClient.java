e2aa820e5d3283a6cfa6cdf31b624f932f8f64c0
migu3lone <mi.gu3lo.ne@gmail.com>
2024-11-19 02:13:01 -0500

    pryClinica

--- app/src/main/java/com/migu3lone/pryclinica/connection/RetrofitClient.java
+++ app/src/main/java/com/migu3lone/pryclinica/connection/RetrofitClient.java
@@ -3,7 +3,7 @@ package com.migu3lone.pryclinica.connection;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
-public class RetrofitClient {//dd
+public class RetrofitClient {
     private static Retrofit retrofit = null;
     private static final String baseUrl = "http://192.168.20.22/pryClinica/";
     //private static final String baseUrl = "http://10.0.2.2/pryClinica/";
